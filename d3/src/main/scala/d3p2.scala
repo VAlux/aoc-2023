91f585bb823d54dc0ff1d19a3bee89e0c0edc9f6
VAlux <olex3000sp@gmail.com>
2023-12-04 21:44:23 +0200

    d3p2 DONE

--- d3/src/main/scala/d3p2.scala
+++ d3/src/main/scala/d3p2.scala
@@ -1,3 +1,26 @@
 import d3p1.*
+import d3p1.SchematicsEntry.PartNumber
+import d3p1.SchematicsEntry.PartSymbol
+import Extensions.*
 object d3p2 extends Solution:
-  override def solve(input: List[String]): Int = 0
+
+  case class Gear(ratioComponents: List[Int]):
+    val ratio = ratioComponents.reduce(_ * _)
+
+  def locateGearSymbols(schematics: List[SchematicsEntry[_]]): List[PartSymbol] =
+    schematics.filter(entry => entry.value == "*").map(_.asInstanceOf[PartSymbol])
+
+  def locateGears(schematics: List[SchematicsEntry[_]]): List[Gear] =
+    val (symbols, parts) = schematics.partition(_.isInstanceOf[PartSymbol])
+    symbols.filter(_.value == '*').flatMap { symbol =>
+      val adjacentParts = parts
+        .filter(part => symbol.adjacentLocations.containsAny(part.bodyLocation))
+        .map(_.asInstanceOf[PartNumber])
+
+      if adjacentParts.size == 2 then Some(Gear(adjacentParts.map(_.value))) else None
+    }
+
+  override def solve(input: List[String]): Int =
+    val schematics = parseEngineSchematics(input)
+    val gears      = locateGears(schematics)
+    gears.map(_.ratio).sum
--- shared/src/main/scala/Extensions.scala
+++ shared/src/main/scala/Extensions.scala
@@ -4,6 +4,9 @@ object Extensions:
     def containsAny(other: Set[A]): Boolean =
       set.exists(other.contains)
 
+    def containsExactly(other: Set[A], amount: Int): Boolean =
+      set.count(other.contains) == amount
+
   extension (list: List[_])
     def tailOrEmpty: List[_] =
       if list.isEmpty then List.empty else list.tail
