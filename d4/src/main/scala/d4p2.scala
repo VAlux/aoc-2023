6c5a913b1f83b8f6dc7e868d5938be07381e2cdd
VAlux <olex3000sp@gmail.com>
2023-12-06 12:42:24 +0200

    d4p2 refactoring

--- d4/src/main/scala/d4p2.scala
+++ d4/src/main/scala/d4p2.scala
@@ -4,15 +4,13 @@ object d4p2 extends Solution:
     extractScoringNumbers(card).size
 
   def copiesWon(cardId: Int, mapping: Map[Int, Int]): List[Int] =
-    val copiesWonAmount = mapping(cardId)
-    if copiesWonAmount == 0 then List.empty
-    else ((cardId + 1) to cardId + copiesWonAmount).toList
+    ((cardId + 1) to cardId + mapping(cardId)).toList
 
   def totalCardsWon(cardId: Int, mapping: Map[Int, Int]): Int =
     val copies = copiesWon(cardId, mapping)
     copies.size + copies.map(copy => totalCardsWon(copy, mapping)).sum
 
   override def solve(input: List[String]): Int =
-    val cards   = input.flatMap(parseCard)
-    val grouped = cards.map(card => card.id -> getScoringNumbersAmount(card)).toMap
-    cards.map(card => totalCardsWon(card.id, grouped)).sum + cards.size
+    val cards        = input.flatMap(parseCard)
+    val scoreMapping = cards.map(card => card.id -> getScoringNumbersAmount(card)).toMap
+    cards.map(card => totalCardsWon(card.id, scoreMapping)).sum + cards.size
