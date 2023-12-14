369df731a4ed54e45fd0a2501c507f2901212c4c
VAlux <olex3000sp@gmail.com>
2023-12-14 09:26:16 +0200

    d5p1 wip

--- d5/src/main/scala/d5p1.scala
+++ d5/src/main/scala/d5p1.scala
@@ -1,8 +1,15 @@
 import scala.annotation.tailrec
 object d5p1 extends Solution:
 
-  case class Mapping(val source: String, val target: String, mapping: Map[Long, Long])
-  case class Almanach(val seeds: List[Long], val mappings: List[Mapping])
+  case class Range(destination: Long, source: Long, size: Long):
+    override def toString(): String = s"$source - $destination : $size"
+
+  case class Mapping(val source: String, val target: String, ranges: List[Range]):
+    override def toString(): String = s"$source -> $target [${ranges.mkString(" | ")}]"
+
+  case class Almanach(val seeds: List[Long], val mappings: List[Mapping]):
+    override def toString(): String =
+      s"${seeds.mkString(" ")}\n${mappings.mkString("\n")}"
 
   def segmentInput(input: List[String]): List[List[String]] =
     @tailrec
@@ -18,10 +25,41 @@ object d5p1 extends Solution:
       case _             => List.empty
 
   def parseAlmanach(input: List[String]): Almanach =
+    def parseSeeds(definition: String): List[Long] =
+      definition.split(":")(1).trim().split(" ").map(_.toLong).toList
+
+    def parseMappings(definition: List[String]): Option[Mapping] =
+      definition.head.split("-") match
+        case Array(from, "to", to) =>
+          val mapping = definition.tail.flatMap { range =>
+            range.split(" ").map(_.toLong) match
+              case Array(destination, source, size) => Some(Range(destination, source, size))
+              case _                                => None
+          }
+
+          Some(Mapping(from, to.replace(" map:", ""), mapping))
+        case _                     => None
+
     val segments = segmentInput(input)
-    segments.foreach(println)
-    ???
+    Almanach(parseSeeds(segments.head.head), segments.tail.flatMap(parseMappings))
+
+  def lookup(source: Long, range: Range): Long =
+    val res =
+      if source >= range.source && source < (range.source + range.size)
+      then range.destination + (source - range.source)
+      else source
+    println(s"source: $source range: $range -> $res")
+    res
+
+  def lookup(source: Long, mapping: Mapping): Long =
+    println(mapping)
+    mapping.ranges.foldLeft(source)(lookup)
+
+  def lookup(source: Long, mappings: List[Mapping]): Long =
+    mappings.foldLeft(source)(lookup)
 
   override def solve(input: List[String]): Int =
-    val almanach = parseAlmanach(input)
-    0
+    val almanach  = parseAlmanach(input)
+    val locations = almanach.seeds.map(seed => lookup(seed, almanach.mappings))
+    println(locations)
+    locations.min.intValue
