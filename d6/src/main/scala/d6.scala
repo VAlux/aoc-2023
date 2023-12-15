2a556c42540441b60be4f9ce4464418b33bf97cd
VAlux <olex3000sp@gmail.com>
2023-12-16 01:58:04 +0200

    d6 scaffolding added

---
+++ d6/src/main/scala/d6.scala
@@ -0,0 +1,22 @@
+@main def entrypoint() =
+  // inputs:
+  val p1TestInput = FileLoader.readFileLines("input-test-p1.txt")
+  val p2TestInput = FileLoader.readFileLines("input-test-p2.txt")
+  val mainInput   = FileLoader.readFileLines("input.txt")
+
+  // test results:
+  val p1TestResult = d6p1.solve(p1TestInput)
+  val p2TestResult = d6p2.solve(p2TestInput)
+
+  // main results:
+  val p1Result = d6p1.solve(mainInput)
+  val p2Result = d6p2.solve(mainInput)
+
+  println("Day 6:")
+  println("-" * 20)
+  println(s"[TEST] P1: $p1TestResult")
+  println(s"[ACTUAL] P1: $p1Result")
+  println("-" * 20)
+  println(s"[TEST] P2: $p2TestResult")
+  println(s"[ACTUAL] P2: $p2Result")
+  println("-" * 20)
---
+++ d6/src/main/scala/d6p1.scala
@@ -0,0 +1,2 @@
+object d6p1 extends Solution:
+  override def solve(input: List[String]): Int = 0
