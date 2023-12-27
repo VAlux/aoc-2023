f09d80f73cbea6470e573de64b9028a9ab63b5aa
VAlux <olex3000sp@gmail.com>
2023-12-27 17:59:31 +0200

    d6p1 resources added, dain day scaffolding updated with arguments parsing

--- d6/src/main/scala/d6.scala
+++ d6/src/main/scala/d6.scala
@@ -4,19 +4,44 @@
   val p2TestInput = FileLoader.readFileLines("input-test-p2.txt")
   val mainInput   = FileLoader.readFileLines("input.txt")
 
-  // test results:
-  val p1TestResult = d6p1.solve(p1TestInput)
-  val p2TestResult = d6p2.solve(p2TestInput)
-
-  // main results:
-  val p1Result = d6p1.solve(mainInput)
-  val p2Result = d6p2.solve(mainInput)
-
   println("Day 6:")
   println("-" * 20)
+
+  args.head match
+    case "test" =>
+      args.last match
+        case "1" => testP1(p1TestInput)
+        case "2" => testP2(p2TestInput)
+        case _   =>
+          testP1(p1TestInput)
+          testP2(p2TestInput)
+    case "main" =>
+      args.last match
+        case "1" => runP1(mainInput)
+        case "2" => runP2(mainInput)
+        case _   =>
+          runP1(mainInput)
+          runP2(mainInput)
+    case _      =>
+      testP1(p1TestInput)
+      testP2(p2TestInput)
+      runP1(mainInput)
+      runP2(mainInput)
+
+  println("-" * 20)
+
+def testP1(input: List[String]) =
+  val p1TestResult = d6p1.solve(input)
   println(s"[TEST] P1: $p1TestResult")
-  println(s"[ACTUAL] P1: $p1Result")
-  println("-" * 20)
+
+def testP2(input: List[String]) =
+  val p2TestResult = d6p2.solve(input)
   println(s"[TEST] P2: $p2TestResult")
+
+def runP1(input: List[String]) =
+  val p1Result = d6p1.solve(input)
+  println(s"[ACTUAL] P1: $p1Result")
+
+def runP2(input: List[String]) =
+  val p2Result = d6p2.solve(input)
   println(s"[ACTUAL] P2: $p2Result")
-  println("-" * 20)
