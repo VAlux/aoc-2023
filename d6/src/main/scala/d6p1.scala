3a83e9521d1aa553f58270cae490d523d33c521c
VAlux <olex3000sp@gmail.com>
2023-12-28 12:44:18 +0200

    d6p2 done

--- d6/src/main/scala/d6p1.scala
+++ d6/src/main/scala/d6p1.scala
@@ -1,22 +1,22 @@
-object d6p1 extends Solution[Int]:
-  case class Round(time: Int, distance: Int)
+object d6p1 extends Solution[BigInt]:
+  case class Round(time: BigInt, distance: BigInt)
 
-  def parseWithPrefix(prefix: String, input: String): List[Int] =
+  def parseWithPrefix(prefix: String, input: String): List[BigInt] =
     input.split(" ").toList.filter(!_.isBlank()) match
-      case prefix :: values => values.map(_.toInt)
+      case prefix :: values => values.map(BigInt(_))
       case _                => List.empty
 
-  def parseTime(input: String): List[Int]     = parseWithPrefix("Time:", input)
-  def parseDistance(input: String): List[Int] = parseWithPrefix("Distance:", input)
+  def parseTime(input: String): List[BigInt]     = parseWithPrefix("Time:", input)
+  def parseDistance(input: String): List[BigInt] = parseWithPrefix("Distance:", input)
 
   def parseRounds(input: List[String]): List[Round] =
     parseTime(input.head).zip(parseDistance(input.last)).map(Round.apply)
 
   def calculateWinningAttemps(round: Round): List[Round] =
-    (0 to round.time)
+    (BigInt(0) to round.time)
       .map(speed => Round(speed, speed * (round.time - speed)))
       .filter(_.distance > round.distance)
       .toList
 
-  override def solve(input: List[String]): Int =
+  override def solve(input: List[String]): BigInt =
     parseRounds(input).map(calculateWinningAttemps).map(_.size).reduce(_ * _)
--- d6/src/main/scala/d6p2.scala
+++ d6/src/main/scala/d6p2.scala
@@ -1,3 +1,16 @@
 import d6p1.*
-object d6p2 extends Solution[Int]:
-  override def solve(input: List[String]): Int = 0
+object d6p2 extends Solution[BigInt]:
+
+  def parseWithPrefix(prefix: String, input: String): BigInt =
+    input.split(" ").toList.filter(!_.isBlank()) match
+      case prefix :: values => BigInt(values.reduce(_ + _))
+      case _                => 0
+
+  def parseTime(input: String): BigInt     = parseWithPrefix("Time:", input)
+  def parseDistance(input: String): BigInt = parseWithPrefix("Distance:", input)
+
+  def parseRound(input: List[String]): Round =
+    Round(parseTime(input.head), parseDistance(input.last))
+
+  override def solve(input: List[String]): BigInt =
+    calculateWinningAttemps(parseRound(input)).size
